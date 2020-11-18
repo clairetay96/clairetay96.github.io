387a2692b48ff364dca6ad2fcee3803bfada9622
clairetay96 <clairetay96@gmail.com>
2020-11-19 00:14:38 +0800

    Updated resume button, script and about margin

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let scrollPosition = document.documentElement.scrollTop
+const scrollPosition = document.documentElement.scrollTop
 
 let aboutPosition = 0
 let skillsPosition = document.getElementById("skills").offsetTop
 let portfolioPosition = document.getElementById("portfolio").offsetTop
 let contactPosition = document.body.offsetHeight - window.innerHeight
 
-window.addEventListener('load', ()=>{
+let scrollSetter = ()=>{
     if(window.scrollY >= aboutPosition && window.scrollY < skillsPosition){
 
     document.getElementById("nav-about").classList.add("current-section")
@@ -33,48 +33,15 @@ window.addEventListener('load', ()=>{
     document.getElementById("nav-contact").classList.remove("current-section")
 
     } else if ( window.scrollY >= contactPosition){
-    console.log("in contact")
+
     document.getElementById("nav-contact").classList.add("current-section")
 
     document.getElementById("nav-portfolio").classList.remove("current-section")
     document.getElementById("nav-skills").classList.remove("current-section")
     document.getElementById("nav-about").classList.remove("current-section")
     }
-})
+}
 
-window.addEventListener('scroll', ()=>{
-    if(window.scrollY >= aboutPosition && window.scrollY < skillsPosition){
+scrollSetter()
 
-        document.getElementById("nav-about").classList.add("current-section")
-
-        document.getElementById("nav-skills").classList.remove("current-section")
-        document.getElementById("nav-portfolio").classList.remove("current-section")
-        document.getElementById("nav-contact").classList.remove("current-section")
-
-
-    } else if ( window.scrollY >= skillsPosition && window.scrollY < portfolioPosition){
-
-        document.getElementById("nav-skills").classList.add("current-section")
-
-        document.getElementById("nav-about").classList.remove("current-section")
-        document.getElementById("nav-portfolio").classList.remove("current-section")
-        document.getElementById("nav-contact").classList.remove("current-section")
-
-
-    } else if (window.scrollY >= portfolioPosition && window.scrollY < contactPosition){
-
-        document.getElementById("nav-portfolio").classList.add("current-section")
-
-        document.getElementById("nav-skills").classList.remove("current-section")
-        document.getElementById("nav-contact").classList.remove("current-section")
-        document.getElementById("nav-about").classList.remove("current-section")
-
-    } else if ( window.scrollY >= contactPosition){
-
-        document.getElementById("nav-contact").classList.add("current-section")
-
-        document.getElementById("nav-portfolio").classList.remove("current-section")
-        document.getElementById("nav-skills").classList.remove("current-section")
-        document.getElementById("nav-about").classList.remove("current-section")
-    }
-})
+window.addEventListener('scroll', scrollSetter)
